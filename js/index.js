1a131f380d79421cc03b95e7e50f560bb673665d
Timoté LANCELLE <timote.lancelle@outlook.fr>
2022-11-24 17:13:29 +0100

    Début archétype HTML, logique algorithme de recherche, mise au propre recipes.js

---
+++ js/index.js
@@ -0,0 +1,41 @@
+import recipes from "../data/recipes.js";
+console.log(recipes)
+
+function getIngredient(recipes) {
+    //Crer arayt vide []
+    for (const recipe of recipes) {
+        //ingrediedts (boucle pour récuepere ingredient)
+            //if(name ingrenet n'existe pas dans mon table -> Ajouter dans le tableau)
+                // push
+    }
+
+    //retrun array
+}
+
+// const ingredientsTag = getIngredient(recipes)
+
+// function getTags(recipes) {
+//     //Crer array ing vide []
+//     //Crer array apl vide []
+//     //Crer array ust vide []
+//     for (const recipe of recipes) {
+//         //ingrediedts (boucle pour récuepere ingredient)
+//             //if(name ingrenet n'existe pas dans mon table -> Ajouter dans le tableau)
+//                 // push
+//         //Appl
+//             //if(name ingrenet n'existe pas dans mon table -> Ajouter dans le tableau)
+//                 // push
+//         //ust
+//             //if(name ingrenet n'existe pas dans mon table -> Ajouter dans le tableau)
+//                 // push
+//     }
+
+//     //retrun {
+//         ingredients : arraying
+//         APL : arrayapl
+//         ust : arrayust
+//     }
+// }
+
+// const allTags = getTags(recipes)
+// const ingredients = allTags.ingredients
