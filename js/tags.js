f3702c5376f5f015eef94d7de3ad7c6d5e0d4c77
Timoté LANCELLE <timote.lancelle@outlook.fr>
2023-02-24 08:33:14 +0100

    Création dynamique des tags, événements, pré-CSS

--- js/tags.js
+++ js/tags.js
@@ -28,13 +28,11 @@ export function getTags(recipes) {
     return tags;
 }
 
-export function init(nameTag, tagList){
-    renderDropdown(nameTag)
-    RenderTagList(nameTag, tagList)
-}
-
-
 function renderDropdown(nameTag) {
+    let tagsListContainer = document.createElement("ul");
+    tagsListContainer.classList.add("display-tagsList", "tagsList-block", `tagsList-block-${nameTag}`);
+    // tagsListContainer.id = `tagsList-block-${nameTag}`;
+
     let dropdownIcon = document.createElement("i");
     dropdownIcon.classList.add("fa-solid", "fa-chevron-down", "fa-lg");
 
@@ -49,28 +47,84 @@ function renderDropdown(nameTag) {
     dropdownInput.ariaLabel = `${nameTag} search`;
 
     let dropdownContainer = document.createElement("div");
-    dropdownContainer.classList.add("col-2", `dropdown-block-${nameTag}`, "noPadding");
+    dropdownContainer.classList.add("col-2", "dropdown-block", `dropdown-block-${nameTag}`, "noPadding");
     dropdownContainer.id = nameTag;
     dropdownContainer.appendChild(dropdownInput);
     dropdownContainer.appendChild(dropdownButton);
+    dropdownContainer.appendChild(tagsListContainer);
 
     let dropdown = document.getElementById("dropdown");
     dropdown.appendChild(dropdownContainer);
 
+    //Ouvrir et fermer la liste de tags
+    dropdownButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        if(tagsListContainer.classList.contains("display-tagsList")) {
+            tagsListContainer.classList.remove("display-tagsList");
+        }else{
+            tagsListContainer.classList.add("display-tagsList");
+        }
+    });
+}
+
+
+    // let openDropdownIcon = document.getElementById(nameTag);
+    // openDropdownIcon.appendChild(tagsContainer);
+
     //addEventListener -> Ouvrir/fermé le menu (Le UL)
+    // let openDropdownIcon = document.getElementById(nameTag);
+    // openDropdownIcon = openDropdownIcon.querySelector("button");
+    // console.log(openDropdownIcon);
+
+
+    // dropdownButton.addEventListener('click', (e) => { RenderTagList(nameTag, tagList) });
+
+function openDropdown(nameTag) {
+    let tagsListContainer = document.getElementById(`tagsList-block-${nameTag}`);
+    tagsListContainer.classList.remove('test')
 }
 
+function closeDropdown(nameTag) {
+    let tagsListContainer = document.getElementById(`tagsList-block-${nameTag}`);
+    tagsListContainer.classList.add('test')
+}
+
+// function RenderTagList(nameTag, tagList){
+//     console.log(tagList, nameTag);
+
+//     let tags = document.createElement("li");
+//     tags.textContent = tagList;
+
+//     let tagsContainer = document.createElement("ul");
+//     tagsContainer.appendChild(tags);
+//     // GetById (section > tag INGREDIENT)
+//     // Création HTML des tags (LI)
+//     //
+// }
+
+// const RenderTagList = (nameTag, tagList) => {
 function RenderTagList(nameTag, tagList){
-    console.log(tagList, nameTag)
-    let elt = document.getElementById(nameTag)
-    elt = elt.querySelector('button')
-    console.log(elt)
-    // GetById (section > tag INGREDIENT)
-    // Création HTML des tags (LI)
-    // 
+    //getElementById pour appendchild le tagsListContainer dans renderDropDown
+    // let tagsListContainer = document.getElementsByClassName(`tagsList-block-${nameTag}`);
+    let tagsListContainer = document.querySelector(`.tagsList-block-${nameTag}`);
+
+    for (const tag of tagList) {
+        let tags = document.createElement("li");
+        tags.textContent = tag;
+        // tagsListContainer[0].appendChild(tags);
+        tagsListContainer.appendChild(tags);
+
+        //Quand on clic sur le tag -> lancer ADDTAG()
+        tagsListContainer.addEventListener('click', (e) => {
+            //Si non e.target.textContent
+            addTag(tags, nameTag)
+        })
+    }
 }
 
-// AddTag (HTML)
+// AddTag (LEs tage en question nom du tag, Catégorie HTML) -> //Close Tag
 
-//Close Tag
-
+export function init(nameTag, tagList){
+    renderDropdown(nameTag)
+    RenderTagList(nameTag, tagList)
+}
